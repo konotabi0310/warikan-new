9140d9a10673463ed35dcac9cffb514a2da0743c
宮川萌花 <miyagawamoeka@miyagawamoehananoMacBook-Air.local>
2025-05-17 19:01:35 +0900

    0517

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,26 +2,19 @@
 export default function Head() {
   return (
     <>
-      {/* PWA マニフェスト */}
+      {/* PWA 用マニフェスト */}
       <link rel="manifest" href="/manifest.json" />
 
-      {/* テーマカラー */}
+      {/* ファビコン */}
+      <link rel="icon" href="/favicon.ico" />
+
+      {/* Android Chrome 用テーマカラー */}
       <meta name="theme-color" content="#FF6B35" />
 
-      {/* ファビコン */}
-      <link rel="icon" href="/icon/icon-192.png" />
-
-      {/* iOS ホーム画面用 */}
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <meta name="apple-mobile-web-app-title" content="ラクワリ" />
+      {/* iOS ホーム画面追加用アイコン */}
       <link rel="apple-touch-icon" href="/icon/icon-192.png" />
-      {/* 必要なら sizes 属性も */}
-      <link
-        rel="apple-touch-icon"
-        sizes="512x512"
-        href="/icon/icon-512.png"
-      />
+      {/* 必要に応じて 512x512 も */}
+      <link rel="apple-touch-icon" sizes="512x512" href="/icon/icon-512.png" />
     </>
   );
 }
